43af3c63fe731c8509d5d1f4b2e67cbfab927931
mateus carvalho <mateus.carvalho@seraph.com.br>
2022-08-21 12:40:42 -0300

    =First Commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Full Cycle Esquenta</h1>"))
+	})
+
+	http.ListenAndServe(":8081", nil)
+}
